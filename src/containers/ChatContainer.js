f67392fb383427af17aa7edc661162cc4bf73ec4
sopascampeao <pekeno123A!>
2019-05-25 12:19:48 +0100

    integration with ui

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import Chat from '../components/Chat/Chat';
-import MessageList from '../components/MessageList';
-import MessageBox from '../components/MessageBox';
-import Header from '../components/Header';
-import firebase from 'firebase';
-
-
 
 const mapStateToProps = ({ alumni }) => {
     return {
@@ -15,33 +9,18 @@ const mapStateToProps = ({ alumni }) => {
 }
 
 class ChatContainer extends Component {
-constructor(props){
-  super(props);
-  var config = {
-    apiKey: "AIzaSyDGGTzAghdMCsCu1deyMps3sCEmn6s8nXY",
-    authDomain: "friendlychat-45e21.firebaseio.com",
-    databaseURL: "https://friendlychat-45e21.firebaseio.com/",
-    projectId: "friendlychat-45e21",
-    storageBucket: "friendlychat-45e21.appspot.com",
-    messagingSenderId: "friendlychat-45e21"
-  };
-  firebase.initializeApp(config);
-}
+    constructor(props) {
+        super(props)
+        this.state = {
+            alumniData: this.props.alumni.data,
+        }
+    }
 
-  render() {
-    return (
-      <div className="container">
-     
-            <div className="columns">
-              <div className="column is-3"></div>
-              <div className="column is-6">
-                <MessageList db={firebase} />
-                <MessageBox db={firebase} />
-              </div>
-            </div> 
-        </div>
-    );
-  }
+    render() {
+        return (
+            <Chat alumni={this.state.alumniData} />
+        )
+    }
 }
 
 export default connect(mapStateToProps)(ChatContainer)
