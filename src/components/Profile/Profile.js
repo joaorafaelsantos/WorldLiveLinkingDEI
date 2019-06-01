f624f217bf561aa7b7c5e7224cd3f7b273aecc1d
arjun2010.ece <arjun2010.ece@gmail.com>
2019-06-01 12:49:00 +0100

    user profile validations complete

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,54 +4,92 @@ import './Profile.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-const Profile = ({ onClick }) => (
+const Profile = (props) => {
+  const {
+      values: { name, location, jobDescription, company, degree },
+      errors,
+      touched,
+      handleSubmit,
+      handleChange,
+      isValid,
+      setFieldTouched
+  } = props;
+
+  const change = (name, e) => {
+    e.persist();
+    handleChange(e);
+    setFieldTouched(name, true, false);
+  };
+  return(
     <div style={{background:'transparent'}}>
-    <div style={{width:400,display:'flex',margin:'40px auto'}}>
-      <Button style={{ marginRight:20,width:183,fontSize:13,padding:'9px 12px' }} variant="contained" color="secondary">
-        Import from Sigarra
-      </Button>
-      <Button style={{ width:183,fontSize:13,padding:'9px 12px' }} variant="contained" color="primary">
-        Import from LinkedIn
-      </Button>
-    </div>
-    <form style={{width:400,display:'flex',flexDirection:'column',
+    <form onSubmit={() => {
+       alert("submitted");
+     }} style={{width:400,display:'flex',flexDirection:'column',
         justifyContent: 'center',margin:'0 auto'}} noValidate autoComplete="off">
+
         <TextField
-          id="standard-full-width"
+          id="name"
+          name="name"
+          helperText={touched.name ? errors.name : ""}
+          error={touched.name && Boolean(errors.name)}
+          value={name}
           style={{ margin: 8 }}
           placeholder="Name"
+          onChange={change.bind(null, "name")}
           margin="normal"
         />
         <TextField
-          id="standard-full-width"
+          id="location"
+          name="location"
+          helperText={touched.location ? errors.location : ""}
+          error={touched.location && Boolean(errors.location)}
+          value={location}
           style={{ margin: 8 }}
           placeholder="Location"
+          onChange={change.bind(null, "location")}
           margin="normal"
         />
          <TextField
-          id="standard-full-width"
+          id="jobDescription"
+          name="jobDescription"
+          helperText={touched.jobDescription ? errors.jobDescription : ""}
+          error={touched.jobDescription && Boolean(errors.jobDescription)}
+          value={jobDescription}
           style={{ margin: 8 }}
           placeholder="Job description"
+          onChange={change.bind(null, "jobDescription")}
           margin="normal"
         />
          <TextField
-          id="standard-full-width"
+          id="company"
+          name="company"
+          helperText={touched.company ? errors.company : ""}
+          error={touched.company && Boolean(errors.company)}
+          value={company}
           style={{ margin: 8 }}
           placeholder="Company"
+          onChange={change.bind(null, "company")}
           margin="normal"
         />
          <TextField
-          id="standard-full-width"
+          id="degree"
+          name="degree"
+          helperText={touched.degree ? errors.degree : ""}
+          error={touched.degree && Boolean(errors.degree)}
+          value={degree}
           style={{ margin: 8 }}
           placeholder="Degree"
+          onChange={change.bind(null, "degree")}
           margin="normal"
         />
-        <Button type="Submit" style={{ marginTop: 40 }} variant="contained" color="secondary">
+        <Button type="Submit" style={{ marginTop: 40 }} variant="contained"
+           color="secondary" disabled={!isValid}>
         Submit
       </Button>
     </form>
-</div>
-);
+  </div>
+ );
+};
 
 Profile.propTypes = {
     onClick: PropTypes.func.isRequired,
--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -3,6 +3,9 @@ import { connect } from "react-redux";
 import { browserHistory } from "react-router-dom";
 import Profile from "../components/Profile/Profile";
 import { authFetchData } from "../actions/auth";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
 
 const mapStateToProps = ({ auth }) => {
   return {
@@ -10,6 +13,22 @@ const mapStateToProps = ({ auth }) => {
   };
 };
 
+const validationSchema = Yup.object({
+  name: Yup.string()
+  .min(3, "Name must contain at least 3 characters")
+  .max(15, "Name can have maximum 15 characters")
+  .required("Name is required"),
+  location: Yup.string("Enter your location")
+  .max(14, "Location can have maximum 14 characters")
+  .required("Location is required"),
+  jobDescription: Yup.string("")
+  .min(8, "Job description must contain at least 8 characters")
+  .max(18, "Job description can have maximum 18 characters"),
+  company: Yup.string("Enter your company name")
+  .max(10, "Company can have maximum 10 characters"),
+  degree: Yup.string("Enter your degree")
+  .required("Degree is required")
+});
 
 class ProfileContainer extends Component {
   constructor(props) {
@@ -25,7 +44,16 @@ class ProfileContainer extends Component {
   }
 
   render() {
-    return <Profile onClick={this.handleClick} />;
+    const values = { name: "", location: "", jobDescription: "", company: "",degree:""};
+    return (
+      <div>
+        <Formik 
+            render={props => <Profile {...props} onClick={this.handleClick} />}
+            initialValues={values}
+            validationSchema={validationSchema}
+        />
+      </div>
+    );
   }
 }
 export default connect(
