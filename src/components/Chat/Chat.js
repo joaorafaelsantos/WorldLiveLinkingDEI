be8fc2ecc95203706468e315ede2f54a62278cc0
Pedro Elias <pedrolge@poli.ufrj.br>
2019-06-02 15:00:39 +0100

    Removed unused variables

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import PropTypes from 'prop-types';
 import ChatMessagesContainer from '../../containers/ChatMessagesContainer'
 import ChatSidebarContainer from '../../containers/ChatSidebarContainer'
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Profile.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
