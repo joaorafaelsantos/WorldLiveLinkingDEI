53b300fe183cde695ac97e489d47050bc3d75a9d
arjun2010.ece <arjun2010.ece@gmail.com>
2019-06-02 18:45:08 +0100

    Added border on chat sidebar and put little margin between sidebar and chatMessages

--- src/components/ChatSidebar/ChatSidebar.js
+++ src/components/ChatSidebar/ChatSidebar.js
@@ -3,7 +3,7 @@ import './ChatSidebar.css';
 
 const ChatSidebar = ({ onClick, users }) => (
     <div className="chat-sidebar-container">
-        <h4>Members</h4>
+        <input type="text" placeholder="Search Members" name="search" className="search-box" />
         <ul className="users-group">
             {users.map((user) => (<li key={user.id} className="chat-sidebar-item" onClick={() => onClick(user.id)}>
             <i class="fas fa-user"></i>
